fe72379d21c26524d1578932978e22f1abe8aa46
Shai Fisher <shaif@balink.net>
2020-11-13 10:01:36 +0200

    Add build folder, replace images

---
+++ build/precache-manifest.dbdcf646a3968ef9eb806aff372a52c9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0efad4600d08c6b042af3a00b27f3d0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5f7d3a303c88a9f361ae",
+    "url": "./static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "661ce9ebade9de609423",
+    "url": "./static/css/main.7edac385.chunk.css"
+  },
+  {
+    "revision": "5f7d3a303c88a9f361ae",
+    "url": "./static/js/2.588cb812.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.588cb812.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "661ce9ebade9de609423",
+    "url": "./static/js/main.89cac55d.chunk.js"
+  },
+  {
+    "revision": "14a35cfea90c458617df",
+    "url": "./static/js/runtime-main.0cb4c1b5.js"
+  },
+  {
+    "revision": "cbf442f11dc5cca699ede7bb9da22d38",
+    "url": "./static/media/curtain.cbf442f1.jpg"
+  }
+]);
